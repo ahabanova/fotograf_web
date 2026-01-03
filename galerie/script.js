b3ca97bd1fe6a0d126fcbb4968d73f3a124d8ab8
ahabanova <ahabanova95@gmail.com>
2026-01-03 12:51:04 +0100

    effect for sections on scroll chnaged to class

--- galerie/script.js
+++ galerie/script.js
@@ -132,8 +132,7 @@ const observerOptions = {
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            entry.target.style.opacity = "1";
-            entry.target.style.transform = "translateY(0)";
+            entry.target.classList.add("visible");
         }
     });
 }, observerOptions);
